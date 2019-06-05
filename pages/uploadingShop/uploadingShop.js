e2f9005a530ffee38426362da1ae5316542d7603
vagrant <1083223866@qq.com>
2019-06-05 16:23:48 +0800

    大改之前的最后一页 0-0

--- pages/uploadingShop/uploadingShop.js
+++ pages/uploadingShop/uploadingShop.js
@@ -1,15 +1,36 @@
 
-
+const shop = ['网商银行', '建设银行', '工商银行', '浦发银行'];
 Page({
   data: {
-    bank: '',
+    linkman: 'vagrant',
+    phone:"17502185988",
+    email:""
   },
-  onSelect() {
+
+  onClassfiySelect(){
+   
+  },
+
+  onShopSelect() {
     my.chooseImage({
-  count: 1,
-  success: (res) => {
-    img.src = res.apFilePaths[0];
+    count: 1,
+    success: (res) => {
+      img.src = res.apFilePaths[0];
+    },
+    });
   },
-  });
+  onBusinessSelect(){
+    my.chooseImage({
+    count: 1,
+    success: (res) => {
+      img.src = res.apFilePaths[0];
+    },
+    });
   },
+
+  submit(){
+    my.switchTab({
+      url:'/pages/shop/home/home'
+    });
+  }
 });
